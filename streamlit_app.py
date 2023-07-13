b553899ad56b00f511bc270037bf47188f5b9974
Kawians <115752969+Kawians@users.noreply.github.com>
2023-07-13 08:32:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,8 @@ model = load_model('rock_paper_scissors_cnn.h5')
 
 # Function to preprocess the image
 def preprocess_image(image):
-    # Read the image file
-    img = cv2.imdecode(np.fromstring(image.read(), np.uint8), 1)
-    # Resize the image to match the input size of your model
-    img = cv2.resize(img, (128, 128))
+    img = cv2.resize(image, (128, 128))
     img = tf.cast(img, tf.float32) / 255.0
-    # Expand dimensions to match the shape expected by the model
     img = np.expand_dims(img, axis=0)
     return img
 
@@ -35,14 +31,15 @@ def main():
 
     # Perform prediction if an image is uploaded
     if uploaded_file is not None:
-        # Predict gesture
-        gesture = predict_gesture(uploaded_file)
-
-        # Read the uploaded file again for display
-        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+        # Read the uploaded file
+        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+        image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
         # Display the uploaded image
-        st.image(image, channels="RGB", use_column_width=True)
+        st.image(image, channels="BGR", use_column_width=True)
+
+        # Predict gesture
+        gesture = predict_gesture(image)
 
         # Display the predicted gesture
         if gesture == 0:
